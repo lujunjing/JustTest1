18b29b6685528adf4d4f42294aa7fabdb7b18a06
JoinJason <370634018@qq.com>
2020-09-04 15:29:35 +0800

    初始化

---
+++ Example/Pods/Target Support Files/JustTest1/JustTest1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JustTest1 : NSObject
+@end
+@implementation PodsDummy_JustTest1
+@end
---
+++ Example/Pods/Target Support Files/Pods-JustTest1_Example/Pods-JustTest1_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JustTest1_Example : NSObject
+@end
+@implementation PodsDummy_Pods_JustTest1_Example
+@end
